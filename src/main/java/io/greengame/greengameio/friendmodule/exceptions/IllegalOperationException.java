c68184c552dbb293ade4e83627523b39ad13f8de
Kacper Kafara <kacperkafara18@gmail.com>
2023-12-09 19:57:09 +0100

    friend module is no more

--- src/main/java/io/greengame/greengameio/friendmodule/exceptions/IllegalOperationException.java
+++
@@ -1,7 +0,0 @@
-package io.greengame.greengameio.friendmodule.exceptions;
-
-public class IllegalOperationException extends RuntimeException{
-    public IllegalOperationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/io/greengame/greengameio/friendmodule/repositories/AbstractChatHolderRepository.java
+++
@@ -1,7 +0,0 @@
-package io.greengame.greengameio.friendmodule.repositories;
-
-import io.greengame.greengameio.friendmodule.model.AbstractChatHolder;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AbstractChatHolderRepository extends MongoRepository<AbstractChatHolder, String> {
-}
