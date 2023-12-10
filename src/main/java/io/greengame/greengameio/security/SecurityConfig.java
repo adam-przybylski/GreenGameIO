376c4ced80b63a571fde5530ea22af86b1d59360
Marcel <kriollik@gmail.com>
2023-12-11 00:10:45 +0100

    feat: add PUT method to cors configuration

--- src/main/java/io/greengame/greengameio/security/SecurityConfig.java
+++ src/main/java/io/greengame/greengameio/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
                 HttpHeaders.ACCEPT));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
