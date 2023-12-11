1370b7fd5baef67bc425d5073343f3dfd7106013
Marcel <kriollik@gmail.com>
2023-12-11 16:09:46 +0100

    feat: add simple sort and change hook in notification page

--- web/src/pages/notifications/index.tsx
+++ web/src/pages/notifications/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useLayoutEffect, useState } from "react";
 import { api } from "../../api/api.config";
 import { Notification } from "../../types/Notification";
 import NotificationModal from "../../components/modals/NotificationModal";
@@ -14,7 +14,7 @@ const Notifications: FC = () => {
     });
   };
 
-  useEffect(() => getNotifications(), []);
+  useLayoutEffect(() => getNotifications());
 
   return (
     <>
@@ -25,22 +25,26 @@ const Notifications: FC = () => {
         <div className="basis-2/12 text-center">AKCJE</div>
       </div>
       <div>
-        {data.map((not) => (
-          <div key={not.id} className="flex justify-between">
-            <div className="basis-2/12 text-center">{not.id}</div>
-            <div className="basis-3/12 text-center">{not.title}</div>
-            <div className="basis-full">{not.content}</div>
-            <div className="basis-2/12 text-center">
-              <Button label="Edit" onClick={() => setNotificationToEdit(not)} />
+        {data
+          .sort((not1, not2) => not1.id - not2.id)
+          .map((not) => (
+            <div key={not.id} className="flex justify-between">
+              <div className="basis-2/12 text-center">{not.id}</div>
+              <div className="basis-3/12 text-center">{not.title}</div>
+              <div className="basis-full">{not.content}</div>
+              <div className="basis-2/12 text-center">
+                <Button
+                  label="Edit"
+                  onClick={() => setNotificationToEdit(not)}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         {notificationToEdit && (
           <NotificationModal
             notification={notificationToEdit}
             reset={() => {
               setNotificationToEdit(undefined);
-              getNotifications();
             }}
           />
         )}
