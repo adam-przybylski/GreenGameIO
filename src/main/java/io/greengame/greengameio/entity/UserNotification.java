c58b40a83786ca381c639a5d560b44259be5ee3e
Marcel <kriollik@gmail.com>
2023-12-11 21:58:18 +0100

    ref: change UserNotification

--- src/main/java/io/greengame/greengameio/entity/UserNotification.java
+++ src/main/java/io/greengame/greengameio/entity/UserNotification.java
@@ -2,21 +2,23 @@ package io.greengame.greengameio.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "user_notifications")
-public class UserNotification extends Notification{
+public class UserNotification extends Notification {
+
     @Column(name = "sended", nullable = false)
-    @NotBlank(message = "Content is mandatory")
+    @NotNull(message = "Content is mandatory")
     private boolean sended;
 
     @ManyToOne
     @JoinColumn(name = "userId", nullable = false)
-    @NotBlank(message = "UserId is mandatory")
+    @NotNull(message = "UserId is mandatory")
     private User user;
 
     public UserNotification(@NotBlank(message = "Title is mandatory") String title, @NotBlank(message = "Content is mandatory") String content, boolean sended, User user) {
@@ -25,10 +27,10 @@ public class UserNotification extends Notification{
         this.user = user;
     }
 
+
     public UserNotification(Long id, @NotBlank(message = "Title is mandatory") String title, @NotBlank(message = "Content is mandatory") String content, boolean sended, User user) {
         super(id, title, content);
         this.sended = sended;
         this.user = user;
     }
-
 }
