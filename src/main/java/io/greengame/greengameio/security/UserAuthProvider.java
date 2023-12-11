d42cd4d2c204d8573e0558dcb468009365f6bc0d
Kacper Kafara <kacperkafara18@gmail.com>
2023-12-11 14:51:19 +0100

    Minor improvements

--- src/main/java/io/greengame/greengameio/security/UserAuthProvider.java
+++ src/main/java/io/greengame/greengameio/security/UserAuthProvider.java
@@ -36,7 +36,7 @@ public class UserAuthProvider {
 
     public String createToken(String login) {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime validity = now.plusSeconds(1);
+        LocalDateTime validity = now.plusHours(1);
         return JWT.create()
                 .withSubject(login)
                 .withIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
