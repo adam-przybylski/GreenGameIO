2ee8ce95100b56455606b808e3225c05b2a8b494
Marcel <kriollik@gmail.com>
2024-01-15 18:37:04 +0100

    feat: add message in user notification page when list of notification is empty

--- web/src/pages/userNotifications/index.tsx
+++ web/src/pages/userNotifications/index.tsx
@@ -49,30 +49,36 @@ const UserNotifications: FC = () => {
         <div className="basis-2/12 text-center">AKCJE</div>
       </div>
       <div className="rounded-b-sm bg-white">
-        {data
-          .sort((not1, not2) => not1.id - not2.id)
-          .map((not) => (
-            <div
-              key={not.id}
-              className="flex justify-between text-black p-2 even:bg-slate-100"
-            >
-              <div className="basis-3/12 flex justify-center items-center">
-                {not.title}
+        {data.length ? (
+          data
+            .sort((not1, not2) => not1.id - not2.id)
+            .map((not) => (
+              <div
+                key={not.id}
+                className="flex justify-between text-black p-2 even:bg-slate-100"
+              >
+                <div className="basis-3/12 flex justify-center items-center">
+                  {not.title}
+                </div>
+                <div className="basis-full flex items-center break-keep">
+                  {not.content}
+                </div>
+                <div className="basis-2/12 flex justify-center items-center text-center">
+                  <Button
+                    label="X"
+                    onClick={() => {
+                      handleRemove(not);
+                    }}
+                    className=" px-2 bg-red-200 p-1 rounded text-black hover:text-white ease-in-out duration-100"
+                  ></Button>
+                </div>
               </div>
-              <div className="basis-full flex items-center break-keep">
-                {not.content}
-              </div>
-              <div className="basis-2/12 flex justify-center items-center text-center">
-                <Button
-                  label="X"
-                  onClick={() => {
-                    handleRemove(not);
-                  }}
-                  className=" px-2 bg-red-200 p-1 rounded text-black hover:text-white ease-in-out duration-100"
-                ></Button>
-              </div>
-            </div>
-          ))}
+            ))
+        ) : (
+          <div className="text-black flex justify-center text-3xl p-3 font-bold">
+            Nie masz powiadomień
+          </div>
+        )}
       </div>
       {showEditPreferences && (
         <NotificationPreferencesModal reset={closePreferencesModal} />
